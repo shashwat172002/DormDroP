70de7b25dccf86b89d89ced01146cc1f18eb94e7
shashwat172002 <shashwatmani17@gmail.com>
2024-03-12 20:50:41 +0530

    Created pages and routes

---
+++ client/src/main.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import ContactUs from "./pages/ContactUs"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/contactus" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
